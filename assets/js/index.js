6e4e3aa82b4788fd475f28f99e49488b08f287a1
DavidTimoteus <2131710012@student.polinema.ac.id>
2024-03-28 10:53:37 +0700

    Penambahan JS/jQUery serta SASS

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,26 @@
-$(document).ready(function(){
-    $(".nav-link").click(function(){
-        // Menghapus kelas "active" dari semua elemen dengan kelas "nav-link"
-        $(".nav-link").removeClass("active");
-        // Menambahkan kelas "active" ke elemen yang diklik
-        $(this).addClass("active");
-        // Mengatur warna teks menjadi merah untuk semua elemen dengan kelas "nav-link"
-        $(".nav-link").css("color", "");
-        // Mengatur warna teks menjadi merah untuk elemen yang diklik
-        $(this).css("color", "red");
+$(document).ready(function () {
+    $(".nav-link").click(function () {
+        $(".nav-link").removeClass("fw-bold active");
+        $(this).addClass("fw-bold active");
     });
+    $(".navbar-brand").mouseenter(function () {
+        $(this).css("color", "#E02424");
+    }).mouseleave(function () {
+        $(this).css("color", "black");
+    });
+
+    var words = $('#text-header').text().split(' ');
+    var index = 0;
+    setInterval(function () {
+        var coloredText = '';
+        for (var i = 0; i < words.length; i++) {
+            if (i === index % words.length) {
+                coloredText += '<span style="color: #E02424">' + words[i] + '</span> ';
+            } else {
+                coloredText += words[i] + ' ';
+            }
+        }
+        $('#text-header').html(coloredText);
+        index++;
+    }, 1000);
 });
